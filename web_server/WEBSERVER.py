c65ce610afba0d78cb3f2915b4973446fb920917
Olivier <orogers@ucsd.edu>
2022-02-18 18:50:12 -0800

    yeet

--- web_server/WEBSERVER.py
+++ web_server/WEBSERVER.py
@@ -66,6 +66,31 @@ def door_querry(req):
     # print(the_response)
     return the_response
 
+def bell_query(req):
+    start_date=req.params['start']
+    end_date=req.params['end']
+    time_zone=req.params['timezone']
+    print(f'start: {start_date}\nend: {end_date}')
+    the_record=querry_db('Bell_Rings',start_date,end_date,time_zone)
+
+    if not the_record:
+        print("no record")
+        return {'id' : "No records."}
+    the_response=[]
+    for item in the_record:
+        # print(item[2])
+        # the_timestamp=item[2]
+        # print(the_timestamp)
+        the_response.append(
+            {
+                'id' : item[0],
+                'ringed' : item[1],
+                'timestamp' : str(item[2])
+            }
+        )
+    # print(the_response)
+    return the_response
+
 def main():
     with Configurator() as config:
         config.add_route('home', '/')
@@ -75,6 +100,8 @@ def main():
         config.add_view(door_override, route_name='lock')
         config.add_route('door', '/door')
         config.add_view(door_querry, route_name='door', renderer='json')
+        config.add_route('bell', '/bell')
+        config.add_view(bell_query, route_name='bell', renderer='json')
 
         # config.add_static_view(name='/', path='/home/pi/repositories/ece-140a-winter-2022-mdlopezme/Lab-6/Midterm/web_server/public', cache_max_age=3600)
 
@@ -88,3 +115,6 @@ def main():
     server = make_server('0.0.0.0', 6543, app)
     print('Web server started on: http://192.168.0.100:6543')
     server.serve_forever()
+
+if __name__ == '__main__':
+    main()
