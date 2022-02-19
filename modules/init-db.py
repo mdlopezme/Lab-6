fb188abbb6139fb9b5668812dd235876ed2231fb
Moises David Lopez Mendoza <mdlopezme@gmail.com>
2022-02-19 00:21:02 -0800

    Don't reset tables if they already exist.

--- modules/init-db.py
+++ modules/init-db.py
@@ -14,7 +14,9 @@ cursor = db.cursor()
 try:
     cursor.execute("CREATE DATABASE Smart_Home;")
 except:
-    print('Database already exists, resetting table Smart_Home')
+    print('Database already exists, exiting')
+    db.close()
+    exit()
 
 cursor.execute("USE Smart_Home;")
 
@@ -40,17 +42,5 @@ try:
     """)
 except RuntimeError as err:
     print('RuntimeError: {err}')
-
-# query = "INSERT INTO Gallery_Details (name,owner,height,age) VALUES (%s,%s,%s,%s)"
-# values = [
-#     ('Geisel-1.jpg','Terrel Gilmore', 163, 45),
-#     ('Geisel-2.jpg','Sila Mann', 189, 51),
-#     ('Geisel-3.jpg','Nyle Hendrix', 152, 27),
-#     ('Geisel-4.jpg','Axel Horton', 176, 21),
-#     ('Geisel-5.jpg','Courtney Mcneil', 172, 64)
-# ]
-
-# cursor.executemany(query,values)
-# db.commit()
 
 db.close()
