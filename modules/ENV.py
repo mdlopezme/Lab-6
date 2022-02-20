029608f1517eedd4a484dbcc11aa97b0821ede3c
Moises David Lopez Mendoza <mdlopezme@gmail.com>
2022-02-19 21:25:04 -0800

    I swear the best commit message ever.

---
+++ modules/ENV.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv('credentials.env')
+
+'''Enviroment Variables'''
+db_host = os.environ['MYSQL_HOST']
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_name = os.environ['MYSQL_DATABASE']
+# public_path = os.environ['PUBLIC_PATH']
+OLIVIER_ROGERS = 397475334654 
+MOISES_LOPEZ = 288461690070
