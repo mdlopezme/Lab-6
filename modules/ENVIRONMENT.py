f9b898883e80a8982099301096aa2d8787856d66
Moises Lopez <mdlopezme@gmail.com>
2022-02-20 13:51:59 -0800

    coments

--- modules/ENVIRONMENT.py
+++ modules/ENVIRONMENT.py
@@ -8,6 +8,7 @@ db_host = os.environ['MYSQL_HOST']
 db_user = os.environ['MYSQL_USER']
 db_pass = os.environ['MYSQL_PASSWORD']
 db_name = os.environ['MYSQL_DATABASE']
-# public_path = os.environ['PUBLIC_PATH']
+
+# Include NFC IDs for all authorized users
 OLIVIER_ROGERS = 397475334654 
 MOISES_LOPEZ = 288461690070
